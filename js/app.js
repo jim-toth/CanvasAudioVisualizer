bac6ba6cbbeed9b2d2d7bbf53d131c83b1c37d34
Jim Toth <gordon.toth@gmail.com>
2015-05-27 13:32:22 -0400

    wat

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 var cav = new CanvasAudioVisualizer('audio');
 var canvas = document.getElementById('canvas');
 var canvasContext = canvasElement.getContext('2d');
+
+var draw = function () {
+	requestAnimationFrame(draw);
+};
+
+cav.playStream();
