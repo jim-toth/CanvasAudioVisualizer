76424d7169e38f3dceed3a9a2186315ed79bd093
Jim Toth <jim@spooky.email>
2015-06-04 21:43:40 -0400

    Update from master.

---
+++ js/cav.js
@@ -0,0 +1,57 @@
+var CanvasAudioVisualizer = function (audio, canvas, options) {
+	var self = this;
+
+	// Grab the audio player element
+	var player = document.getElementById(audio);
+	
+	// Grab the audio context safely
+	var audioContext = new (window.AudioContext || window.webkitAudioContext);
+
+	// Grab the audio source and create an analyser
+	var source = audioContext.createMediaElementSource(player);
+	var analyser = audioContext.createAnalyser();
+
+	// Grab and set fftSize override from options if it exists
+	if(typeof options != "undefined" && typeof options.fftSize != "undefined") {
+		analyser.fftSize = options.fftSize;
+	} else {
+		analyser.fftSize = 512; //default to 512
+	}
+
+	// Wire the audio source => analyser => destination (speakers)
+	source.connect(analyser);
+	analyser.connect(audioContext.destination);
+
+	// Grab the canvas and the canvas context
+	var canvas = document.getElementById(canvas);
+	var canvasContext = canvas.getContext('2d');
+
+	// Simple function to grab the current FFT data from the audio
+	var sampleAudio = function () {
+		analyser.getByteFrequencyData(self.streamData);
+	};
+
+	// Draw function
+	var draw = function () {
+		// Set up loop
+		requestAnimationFrame(draw);
+
+		// Update streamData
+		sampleAudio();
+
+		// Fire animate callback function, if it exists, to update the canvas
+		if(typeof options != "undefined" && typeof options.animateFn == "function") {
+			options.animateFn(canvas, canvasContext, self.streamData);
+		}
+	};
+
+	// Initialize stream data
+	self.streamData = new Uint8Array(analyser.fftSize/2);
+
+	// Define play function to begin playback, animation loop, and analysis
+	self.play = function (streamUri) {
+		player.setAttribute('src', streamUri);
+		player.play();
+		draw();
+	};
+};
